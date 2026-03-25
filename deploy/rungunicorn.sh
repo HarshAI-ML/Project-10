ebc8f5baa87365b22f0d53abd251ae9e7331057f
Hexii <abuhuzaifakurla@gmail.com>
2026-03-25 11:33:58 +0530

    fixed runguicorn.sh

--- deploy/rungunicorn.sh
+++ deploy/rungunicorn.sh
@@ -8,10 +8,7 @@ SOCKET_PATH="${SOCKET_PATH:-$BACKEND_DIR/project10.sock}"
 cd "$BACKEND_DIR"
 
 if [ -f "$BACKEND_DIR/.env" ]; then
-  set -a
-  # shellcheck disable=SC1090
-  source "$BACKEND_DIR/.env"
-  set +a
+  echo "Found $BACKEND_DIR/.env (Django will load it via python-dotenv)."
 fi
 
 source "$BACKEND_DIR/venv/bin/activate"
